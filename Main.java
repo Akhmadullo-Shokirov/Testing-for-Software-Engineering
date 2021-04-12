721d8f9518faca954e8f3850ba31d8c08df00b17
Axmadullo <66511606+Axmadullo@users.noreply.github.com>
2021-04-12 08:28:53 +0200

    Update Main.java
    the java code for checking if quadratic equation has only one root, two roots or no roots.

--- Main.java
+++ Main.java
@@ -8,30 +8,17 @@ public class Main {
         int c = 3;
 
         Main main = new Main();
-        double resultDiscriminant = main.calculateD(a, b, c);
-        System.out.println("D = " + resultDiscriminant);
-        main.calculateRoots(a, b, resultDiscriminant);
-
-
+        System.out.println(main.calculateRoots(a, b, c));
     }
 
-    private double calculateD(int a, int b, int c) {
-        return b*b - (4 * a * c);
-    }
-
-    private void calculateRoots(int a, int b, double d) {
+    public String calculateRoots(int a, int b, int c) {
+        double d = b*b -(4 * a * c);
         if(d < 0) {
-            System.out.println("No real roots");
+            return "No real roots";
         } else if(d == 0) {
-            System.out.println("Only one real root");
+            return "Only one real root";
         } else {
-            System.out.println("Two real roots");
-            double rootOne = (-b + Math.sqrt(d))/(2*a);
-            double rootTwo = (-b - Math.sqrt(d))/(2*a);
-            System.out.println("root 1 = " + rootOne);
-            System.out.println("root 2 = " + rootTwo);
+            return "Two real roots";
         }
     }
-
-
 }
