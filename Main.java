a5d11cb7fe3ffe86073a3193fca65b15eb7b12d5
Axmadullo <66511606+Axmadullo@users.noreply.github.com>
2021-04-12 09:25:09 +0200

    Update Main.java
    In this case the code tells about how many roots quadratic equation has or not, and prints those roots in the console.

--- Main.java
+++ Main.java
@@ -8,16 +8,25 @@ public class Main {
         int c = 3;
 
         Main main = new Main();
-        System.out.println(main.calculateRoots(a, b, c));
+        main.calculateRoots(a, b, c);
     }
-
+    /** Method for calculating the roots of quadratic equation */
+    
     public String calculateRoots(int a, int b, int c) {
         double d = b*b -(4 * a * c);
         if(d < 0) {
-            return "No real roots";
+            String result = "No real roots";
+            System.out.println(result);
+            return result;
         } else if(d == 0) {
+            double root = -(b/(2*a));
+            System.out.println("root = " + root);
             return "Only one real root";
         } else {
+            double rootOne = (-b + Math.sqrt(d))/(2*a);
+            double rootTwo = (-b - Math.sqrt(d))/(2*a);
+            System.out.println("root 1 = " + rootOne);
+            System.out.println("root 2 = " + rootTwo);
             return "Two real roots";
         }
     }
